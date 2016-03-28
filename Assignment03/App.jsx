11da2990b06805313ae6cd3197cf52c03a360897
HerrR <rbergel@kth.se>
2016-03-28 12:52:48 +0800

    Initial Commit

---
+++ Assignment03/App.jsx
@@ -0,0 +1,15 @@
+// App component - represents the whole app
+App = React.createClass({
+  
+  // mixins: [ReactMeteorData],
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>CS3249 Assignment03</h1>
+        </header>
+      </div>
+    );
+  }
+});
