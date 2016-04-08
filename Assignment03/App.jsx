2f215d18f8d0c0449ed93c6125771d5898d5ce28
HerrR <rbergel@kth.se>
2016-04-08 16:39:15 +0800

    Bootstrap, Fontawesome and Table Layout
    - Added Bootstrap
    - Added Fontawesome
    - Tried to copy table layout from IVLE as much as possible
    
    Next step:
    - Implement states

--- Assignment03/App.jsx
+++ Assignment03/App.jsx
@@ -4,15 +4,47 @@ App = React.createClass({
  
   render() {
     var randomEvents = [
-      { _id: 1, title: "Something ", organiser: "NUS Students", eventDate: "First week of april" },
-      { _id: 2, title: "Something else", organiser: "Rickard", eventDate: "Second week of april" },
-      { _id: 3, title: "Something entirely different ", organiser: "Katie", eventDate: "Last week of april" }
+      { 
+        _id: 1, 
+        title: "Something ", 
+        organiser: "NUS Students", 
+        eventDate: "First week of april",
+        tags: ["tag1", "tag2", "tag3"],
+        description: "An event about something",
+        venue: "Somewhere",
+        price: "5$",
+        agenda: "First we do something, then we do something else",
+        hits: 100
+      },
+      { 
+        _id: 2, title: "Something else", 
+        organiser: "Rickard", 
+        eventDate: "Second week of april",
+        tags: ["tag1", "tag2", "tag3"],
+        description: "An event about something else",
+        venue: "Somewhere",
+        price: "5$",
+        agenda: "First we do something, then we do something else",
+        hits: 100
+      },
+      { 
+        _id: 3, title: "Something entirely different ", 
+        organiser: "Katie", 
+        eventDate: "Last week of april",
+        tags: ["tag1", "tag2", "tag3"],
+        description: "An event about something entirely different",
+        venue: "Somewhere",
+        price: "5$",
+        agenda: "First we do something, then we do something else",
+        hits: 100
+      }
     ];
     console.log("Props in App", this.props);
     return (
       <div className="container">
         <header>
           <h1>CS3249 Assignment03</h1>
+          <div className="contributors">Rickard Bergeling, Katie Huang</div>
         </header>
         <EventTable events={randomEvents} />
       </div>
--- Assignment03/EventTable.jsx
+++ Assignment03/EventTable.jsx
@@ -1,7 +1,7 @@
 EventTable = React.createClass({
 	renderEvents(){
 		var events = this.props.events.map(function(ev){
-			return <Event key={ev.id} details={ev} />;
+			return <Event key={ev._id} details={ev} />;
 		});
 		return events;
 	},
@@ -10,8 +10,41 @@ EventTable = React.createClass({
 		console.log("Props in EventTable", this.props);
 		return (
 				<div className="eventTable">
-				<h2>This is the event table, stuff should be rendered below!</h2>
-					{this.renderEvents()}
+					<div className="row mainActions">
+						<div className="col-md-12">
+							<span className="col-md-3">
+								<button className="btn btn-success">
+									<span className="glyphicon glyphicon-plus"></span>
+									&nbsp;Create Event
+								</button>
+							</span>
+							<span className="col-md-6">
+								<div className="input-group">
+									<input type="text" className="form-control" placeholder="Search"/>
+									<span className="input-group-btn">
+										<a className="btn btn-default"><span className="glyphicon glyphicon-search" style={{color: 'rgb(51,51,51)'}}></span></a>
+										<a className="btn btn-default"><span className="fa fa-undo" style={{color: 'rgb(51,51,51)'}}></span></a>
+									</span>
+								</div>
+							</span>
+							<span className="col-md-3 text-right form-inline">
+								<div className="navbar-btn">
+									Total {this.props.events.length} items&nbsp;&nbsp;&nbsp;&nbsp;
+								</div>
+							</span>
+						</div>
+					</div>
+					<table className="table table-hover table-striped">
+						<tbody>
+							<tr>
+								<th>View</th>
+								<th>Title</th>
+								<th>Event Organizer</th>
+								<th>Event Date</th>
+							</tr>
+							{this.renderEvents()}
+						</tbody>
+					</table>
 				</div>
 			)
 	}
@@ -20,13 +53,14 @@ EventTable = React.createClass({
 Event = React.createClass({
 	render(){
 		console.log("Props in event",this.props);
+		var eventDetails = this.props.details;
 		return (
-			<div className="event">
-				<h3>This is an event </h3>
-				<div>Organiser: {this.props.details.organiser}</div>
-				<div>Title: {this.props.details.title}</div>
-				<div>Date: {this.props.details.eventDate}</div>
-			</div>
+			<tr>
+				<td>View Icon</td>
+				<td>{eventDetails.title}</td>
+				<td>{eventDetails.organiser}</td>
+				<td>{eventDetails.eventDate}</td>
+			</tr>
 		)
 	}
 });
