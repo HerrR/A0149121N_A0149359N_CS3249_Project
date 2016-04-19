1ca5efa71a4a210608d46cf7c092bac852f382f5
katiehuang <huang.katie94@gmail.com>
2016-04-19 19:44:15 +0700

    installed testing dependencies

---
+++ Assignment03/imports/test/test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+import Register from '../imports/ui/Register.jsx';
+
+const register = () => 
+
+describe('register', function(){
+	beforeEach(() => {
+		browser.url('http://localhost:3000');
+	})
+})
